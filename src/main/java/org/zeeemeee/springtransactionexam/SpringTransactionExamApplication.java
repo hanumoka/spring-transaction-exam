ffc2c8fa8bf3e54d29cfeb1e17aaa37dc3dfbc50
hanumoka <amagramer@gmail.com>
2025-02-27 19:45:25 +0900

    1

---
+++ src/main/java/org/zeeemeee/springtransactionexam/SpringTransactionExamApplication.java
@@ -0,0 +1,13 @@
+package org.zeeemeee.springtransactionexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTransactionExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zeeemeee/springtransactionexam/SpringTransactionExamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zeeemeee.springtransactionexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
